ead2c6d6feb93c0c10ae3933408e8a361a5072d6
kjohna <43828763+kjohna@users.noreply.github.com>
2019-08-29 17:28:29 -0600

    BUG map is getting messed up, commented out map updating. runs fine otherwise

--- src/explorer.py
+++ src/explorer.py
@@ -80,6 +80,12 @@ class Explorer:
         elif req_type == 'sell':
             r = requests.post(self.server_url + '/sell/',
                               headers=self.auth_header, data=data_json)
+        elif req_type == 'dash':
+            r = requests.post(self.server_url + '/dash/',
+                              headers=self.auth_header, data=data_json)
+        elif req_type == 'fly':
+            r = requests.post(self.server_url + '/fly/',
+                              headers=self.auth_header, data=data_json)
         else:
             print('unknown req_type')
         r_data = r.json()
@@ -118,11 +124,13 @@ class Explorer:
             self.map_graph[self.current_room] = {
                 'title': self.current_room_title, 'exits': found_exits
             }
-        self.map_graph[prev_room]['exits'][dir_traveled] = self.current_room
-        self.map_graph[self.current_room]['exits'][rev_dir] = prev_room
-        self.map_graph[self.current_room]['elevation'] = r_data['elevation']
-        self.map_graph[self.current_room]['terrain'] = r_data['terrain']
-        self.map_graph[self.current_room]['coordinates'] = r_data['coordinates']
+        # TODO: theres something messing up the map_graph, check where orient is called in travel??
+        if len(self.map_graph.keys()) < 500:
+            self.map_graph[prev_room]['exits'][dir_traveled] = self.current_room
+            self.map_graph[self.current_room]['exits'][rev_dir] = prev_room
+            self.map_graph[self.current_room]['elevation'] = r_data['elevation']
+            self.map_graph[self.current_room]['terrain'] = r_data['terrain']
+            self.map_graph[self.current_room]['coordinates'] = r_data['coordinates']
         self.update_stored_map()
 
     def update_stored_map(self):
@@ -174,56 +182,87 @@ class Explorer:
         print(f"new route: {travel_queue}")
         return travel_queue
 
-    def travel(self, travel_queue):
+    def travel(self, travel_queue, fast=False):
+        '''
+        expects a travel_queue of type: "collections.deque"
+        travels according to the directions in travel_queue
+        subject to some constraints:
+        1) if we're over encumbered, clear travel queue and
+        set self.encumbered to true. this triggers re-routing to the shop to sell items
+        '''
         # expects a travel_queue which is a "deque"
         while len(travel_queue) > 0:
             direction = travel_queue.popleft()
+            print(f"nxt route: {travel_queue}")
             next_room = self.map_graph[self.current_room]['exits'][direction]
             data = {'direction': direction}
-            # # as long as we're going in a straight line we can dash
-            # if len(travel_queue) > 0 and direction == travel_queue[0]:
-            #     print("can dash!")
-            #     next_room_ids = [next_room]
-            #     while len(travel_queue) > 0 and direction == travel_queue[0]:
-            #         direction = travel_queue.popleft()
-            #         next_room = self.map_graph[next_room]['exits'][direction]
-            #         next_room_ids.append(next_room)
-            #     # handle case we're traveling to a '?'
-            #     if next_room_ids[len(next_room_ids) - 1] == '?':
-            #         next_room_ids.pop()
-            #     room_count = len(next_room_ids)
-            #     #'{"direction":"n", "num_rooms":"5", "next_room_ids":"10,19,20,63,72"}'
-            #     data['num_rooms'] = room_count
-            #     data['next_room_ids'] = ",".join(next_room_ids)
-            #     data_json = json.dumps(data)
-            #     print(f"dash: {data_json}")
-            #     r = requests.post(self.server_url + '/dash/',
-            #                       headers=self.auth_header, data=data_json)
-            # else:
-            # not a straight line
-            # if we know the id of the next room, add to the data for the request to get "Wise Explorer" reduction of cooldown
+            # if we know id of next room, add to request data to get "Wise Explorer" reduction of cooldown
             if next_room != '?':
                 data['next_room_id'] = next_room
-            r_data = self.make_request('move', data)
+            # if we're fast traveling:
+            if fast:
+                # as long as we're going in a straight line we can dash
+                # NOTE: only after changing name and praying at 461
+                if len(travel_queue) > 0 and direction == travel_queue[0]:
+                    print("can dash!")
+                    next_room_ids = [next_room]
+                    while len(travel_queue) > 0 and direction == travel_queue[0]:
+                        direction = travel_queue.popleft()
+                        next_room = self.map_graph[next_room]['exits'][direction]
+                        next_room_ids.append(next_room)
+                    # handle case we're traveling to a '?'
+                    if next_room_ids[len(next_room_ids) - 1] == '?':
+                        next_room_ids.pop()
+                    room_count = len(next_room_ids)
+                    #'{"direction":"n", "num_rooms":"5", "next_room_ids":"10,19,20,63,72"}'
+                    data['num_rooms'] = str(room_count)
+                    data['next_room_ids'] = ",".join(next_room_ids)
+                    # also remove 'next_room_id'
+                    del data['next_room_id']
+                    r_data = self.make_request('dash', data)
+                    print(f"dash response: {r_data}")
+                    # data_json = json.dumps(data)
+                    # print(f"dash: {data_json}")
+                    # r = requests.post(self.server_url + '/dash/',
+                    #                   headers=self.auth_header, data=data_json)
+                else:
+                    # can't dash
+                    r_data = self.make_request('move', data)
+                self.orient(r_data, direction)
+            # not fast traveling
+            else:
+                # if the next room has elevated terrain, fly!
+                # NOTE: only after changing name and praying at 22
+                terrain = self.map_graph[next_room]['terrain']
+                if terrain == 'MOUNTAIN':
+                    print("FLYING")
+                    r_data = self.make_request('fly', data)
+                else:
+                    r_data = self.make_request('move', data)
+                # # code to pick up all treasure in the room
+                while len(r_data['items']) > 0 and not self.encumbered:
+                    r_data = self.make_request('take', {'name': 'treasure'})
+                    print("$" * 20)
+                    print(f"treasure found: {r_data}")
             self.orient(r_data, direction)
             print("-" * 20)
-            print(f"travel request: {r_data}")
+            print(f"travel response: {r_data}")
             # clear travel_queue and set self.encumbered if we are over encumbered an not aware
             if not self.encumbered and "Heavily Encumbered: +100% CD" in r_data['messages']:
                 self.encumbered = True
                 travel_queue = []
-            # # code to pick up all treasure in the room
-            while len(r_data['items']) > 0 and not self.encumbered:
-                r_data = self.make_request('take', {'name': 'treasure'})
-                print("$" * 20)
-                print(f"treasure found: {r_data}")
 
     def explore(self):
+        '''
+        run until interrupted
+        1) check self.map_graph, if there are '?', travel there, else travel to a random room
+        2) if encumbered, travel to the shop and sell items
+        '''
         while True:
             if not self.encumbered:
                 # keep exploring until over encumbered by treasure
                 # search for a room...
-                # travel_queue = self.get_route_to('66')
+                # travel_queue = self.get_route_to('77')
                 # ... or travel a random direction
                 # travel_queue = collections.deque([random.choice(self.exits)])
                 # ... or travel to a random room
@@ -236,7 +275,7 @@ class Explorer:
                 # dump one treasure first
                 r_data = self.make_request('drop', {"name": "treasure"})
                 travel_queue = self.get_route_to('1')
-                self.travel(travel_queue)
+                self.travel(travel_queue, True)
                 print("Arrived at shop, sell treasures!")
                 # figure out how many treasures we have
                 r_data = self.make_request('status')
